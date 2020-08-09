14b9f740254dd6a9d7a0a62b9d41c6c5b5834b40
Sychic <Sychic@github.com>
2020-08-09 11:20:50 -0400

    verification and database storage

--- bot.js
+++ bot.js
@@ -1,13 +1,35 @@
 require('dotenv').config();
 const fs = require('fs');
+const mongodb = require('mongodb');
 const Discord = require('discord.js');
 global.colors = require("colors");
+const verify = require('./verify.js');
+const channels = JSON.parse(process.env.CHANNEL);
 
 const client = new Discord.Client();
+const mongo = new mongodb.MongoClient(process.env.DB, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	});
+try {
+	mongo.connect();
+} catch (error) {
+	console.error(error);
+}
+module.exports.mongo = mongo;
 
 client.once('ready', () => {
 	client.user.setActivity(`${client.users.cache.filter(user => !user.bot ).size} users`, { type: 'WATCHING' });
 	console.log((`Mee6 overrated, dyno outdated, ${client.user.username}™ activated`).rainbow);
 });
+
+client.on("message",message=>{
+	if(message.author.bot) return;
+	if(message.guild&&channels.includes(message.channel.id)){
+		console.log(message.cleanContent);
+		verify(message.cleanContent,message);
+	}
+
+})
 
 client.login(process.env.TOKEN);
