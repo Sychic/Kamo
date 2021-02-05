fc3e98bad5c2114f432ac2cc5631a292cc495473
Sychic <Sychic@github.com>
2021-02-04 21:19:41 -0500

    cleanup

--- commands/Guild/uuidban.js
+++ commands/Guild/uuidban.js
@@ -7,11 +7,6 @@ module.exports = {
     execute (message) {
         let targetMember = message.mentions.users.first();
         if(!targetMember) return message.reply('you need to tag a user!');
-        message.reply(targetMember);
-            ban(message, targetMember);
-            // message goes below!
-            // message.channel.send(`<@${targetMember.user.id}> you just got a hug  https://tenor.com/view/anime-cuddle-cute-gif-12668750`);
-        
-
+        ban(message, targetMember);
     }
 }
