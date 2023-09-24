511386e4e4bd418ebf7de49f2413771432eca57a
sychic <47618543+Sychic@users.noreply.github.com>
2023-09-23 20:01:52 -0400

    Implement XboxLive Auth

---
+++ src/main/kotlin/kamo/bridge/auth/XboxLive.kt
@@ -0,0 +1,81 @@
+package kamo.bridge.auth
+
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kamo.bridge.auth.Auth.httpClient
+import kotlinx.datetime.Instant
+import kotlinx.serialization.json.*
+
+suspend fun obtainXBLToken(accessToken: String) =
+    httpClient.post {
+        url {
+            protocol = URLProtocol.HTTPS
+            host = "user.auth.xboxlive.com"
+            path("user", "authenticate")
+        }
+        contentType(ContentType.Application.Json)
+        setBody(
+            JsonObject(mapOf(
+                "Properties" to JsonObject(mapOf(
+                    "AuthMethod" to JsonPrimitive("RPS"),
+                    "SiteName" to JsonPrimitive("user.auth.xboxlive.com"),
+                    "RpsTicket" to JsonPrimitive("d=$accessToken")
+                )),
+                "RelyingParty" to JsonPrimitive("http://auth.xboxlive.com"),
+                "TokenType" to JsonPrimitive("JWT")
+            ))
+        )
+    }.body<JsonObject>().xblToken()
+
+private fun JsonObject.xblToken(): XBLToken {
+    val token = get("Token")?.jsonPrimitive?.content ?: throw IllegalStateException("Missing XBL token")
+    val userhash = get("DisplayClaims")?.jsonObject?.
+        get("xui")?.jsonArray?.
+        get(0)?.jsonObject?.get("uhs")?.
+        jsonPrimitive?.content ?: throw IllegalStateException("Malformed xbl token data: missing user hash")
+    val expiry = get("NotAfter")?.jsonPrimitive?.content ?: throw IllegalStateException("Malformed xbl token data: missing expiration")
+    return XBLToken(token, userhash, Instant.parse(expiry))
+}
+
+data class XBLToken(
+    val token: String,
+    val userHash: String,
+    val expiry: Instant
+)
+
+suspend fun obtainXSTSToken(xblToken: XBLToken) =
+    httpClient.post {
+        url {
+            protocol = URLProtocol.HTTPS
+            host = "xsts.auth.xboxlive.com"
+            path("xsts", "authorize")
+        }
+        contentType(ContentType.Application.Json)
+        setBody(
+            JsonObject(mapOf(
+                "Properties" to JsonObject(mapOf(
+                    "SandboxId" to JsonPrimitive("RETAIL"),
+                    "UserTokens" to JsonArray(listOf(JsonPrimitive(xblToken.token)))
+                )),
+                "RelyingParty" to JsonPrimitive("rp://api.minecraftservices.com/"),
+                "TokenType" to JsonPrimitive("JWT")
+            ))
+        )
+    }.body<JsonObject>().xstsToken()
+
+private fun JsonObject.xstsToken(): XSTSToken {
+    val token = get("Token")?.jsonPrimitive?.content ?: throw IllegalStateException("Missing xsts token")
+    val userhash = get("DisplayClaims")?.jsonObject?.
+    get("xui")?.jsonArray?.
+    get(0)?.jsonObject?.get("uhs")?.
+    jsonPrimitive?.content ?: throw IllegalStateException("Malformed xsts token data: missing user hash")
+    val expiry = get("NotAfter")?.jsonPrimitive?.content ?: throw IllegalStateException("Malformed xsts token data: missing expiration")
+    return XSTSToken(token, userhash, Instant.parse(expiry))
+}
+
+data class XSTSToken(
+    val token: String,
+    val userHash: String,
+    val expiry: Instant
+)
--- src/test/kotlin/kamo/Oauth.kt
+++ src/test/kotlin/kamo/Oauth.kt
@@ -2,6 +2,8 @@ package kamo
 
 import io.kotest.matchers.shouldBe
 import kamo.bridge.auth.authorizeDevice
+import kamo.bridge.auth.obtainXBLToken
+import kamo.bridge.auth.obtainXSTSToken
 import kamo.bridge.auth.pollUserAuth
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.runTest
@@ -22,5 +24,27 @@ object Oauth {
         val authData = pollUserAuth(data)
         authData.token_type shouldBe "Bearer"
         println(authData)
+    }
+
+    @Test
+    fun testXboxLiveAuth() = runBlocking {
+        val data = authorizeDevice()
+        println(data.message)
+        val authData = pollUserAuth(data)
+        println(authData)
+        val xblData = obtainXBLToken(authData.access_token)
+        println(xblData)
+    }
+
+    @Test
+    fun testXsts() = runBlocking {
+        val data = authorizeDevice()
+        println(data.message)
+        val authData = pollUserAuth(data)
+        println(authData)
+        val xblData = obtainXBLToken(authData.access_token)
+        println(xblData)
+        val xstsData = obtainXSTSToken(xblData)
+        println(xstsData)
     }
 }
