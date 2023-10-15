7d6cfbd9d3d551504132c17d001d57177489f2f6
sychic <47618543+Sychic@users.noreply.github.com>
2023-10-15 15:04:54 -0400

    Add way to get mc profile data

--- src/main/kotlin/kamo/bridge/auth/Minecraft.kt
+++ src/main/kotlin/kamo/bridge/auth/Minecraft.kt
@@ -6,8 +6,10 @@ import io.ktor.http.*
 import kamo.Kamo.httpClient
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
+import java.util.*
 
 suspend fun obtainMCToken(xstsToken: XSTSToken) =
     httpClient.post {
@@ -24,6 +26,16 @@ suspend fun obtainMCToken(xstsToken: XSTSToken) =
         )
     }.body<MCTokenData>()
 
+suspend fun getMCProfile(access_token: String) =
+    httpClient.get {
+        url {
+            protocol = URLProtocol.HTTPS
+            host = "api.minecraftservices.com"
+            path("minecraft", "profile")
+        }
+        bearerAuth(access_token)
+    }.body<MCProfileData>()
+
 @Serializable
 data class MCTokenData(
     @SerialName("username")
@@ -31,3 +43,12 @@ data class MCTokenData(
     val access_token: String,
     val token_type: String
 )
+
+@Serializable
+data class MCProfileData(
+    val id: String,
+    val name: String
+) {
+    @Transient
+    val uuid = id.chunked(id.length / 2).map { it.toULong(16).toLong() }.let { (first, second) -> UUID(first, second) }
+}
